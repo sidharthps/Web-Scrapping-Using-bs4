d9119586d21faa1e1f8750bd40b510955c382999
Sidharth P S <sidharthpsnirmallur@gmail.com>
2021-03-31 16:45:27 -0400

    Using Scrapy

---
+++ post_scrape.py
@@ -0,0 +1,24 @@
+import scrapy
+
+class Post_Spider(scrapy.spider):
+    name = "posts"
+    start_urls = [
+        'https://www.reddit.com/r/gameofthrones/'
+    ]
+
+    def parse(self, response):
+        # Extracting the content using css selectors
+        titles = response.css('.title.may-blank::text').extract()
+        votes = response.css('.score.unvoted::text').extract()
+        times = response.css('time::attr(title)').extract()
+        comments = response.css('.comments::text').extract()
+
+        # Give the extracted content row wise
+        for item in zip(titles, votes, times, comments):
+            # create a dictionary to store the scraped info
+            scraped_info = {
+                'title': item[0],
+                'vote': item[1],
+                'created_at': item[2],
+                'comments': item[3],
+            }
